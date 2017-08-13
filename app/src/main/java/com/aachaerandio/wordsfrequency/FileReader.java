054190f9ea15e9fa49fc1c785d6a72ec9e6000d2
Araceli Achaerandio <araceli.achaerandio@gmail.com>
2017-08-13 11:27:09 +0100

    Adding tests for FileReader

--- app/src/main/java/com/aachaerandio/wordsfrequency/FileReader.java
+++ app/src/main/java/com/aachaerandio/wordsfrequency/FileReader.java
@@ -1,12 +1,17 @@
 package com.aachaerandio.wordsfrequency;
 
+import java.io.InputStream;
+
 public class FileReader {
 
     public FileReader(String fileName) {
         //TODO Open file
+        //InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(fileName);
     }
 
     public String readLine() {
         return null;
     }
+
+    public void close(){}
 }
--- app/src/test/java/com/aachaerandio/wordsfrequency/FileReaderTest.java
+++ app/src/test/java/com/aachaerandio/wordsfrequency/FileReaderTest.java
@@ -10,29 +10,38 @@ public class FileReaderTest {
 
     @Test
     public void should_open_file_successfully() throws Exception {
-
+        FileReader fileReader = new FileReader("test_one_line.txt");
+        assertTrue(true);
+        fileReader.close();
     }
 
     @Test
     public void should_read_line_successfully() throws Exception {
-
+        FileReader fileReader = new FileReader("test_one_line.txt");
+        String line = fileReader.readLine();
+        assertEquals("hello world", line);
+        fileReader.close();
     }
 
     @Test
     public void should_read_empty_file_successfully() throws Exception {
-
+        FileReader fileReader = new FileReader("test_empty.txt");
+        String line = fileReader.readLine();
+        assertNull(line);
+        fileReader.close();
     }
 
     @Test(expected = FileNotFoundException.class)
     public void should_throw_exception_when_file_does_not_exist() throws Exception {
-
+        new FileReader("test_not_exist.txt");
     }
 
     @Test
     public void should_return_lines_with_at_least_one_word() throws Exception {
-
+        FileReader fileReader = new FileReader("test_ignorable_content.txt");
+        String line = fileReader.readLine();
+        assertEquals("hello world", line);
+        fileReader.close();
     }
-
-
 
 }
